0274d966c8a626bb2be349b65744cbfa92376886
Harsh Jaiswal <145054490+hjaiswal99@users.noreply.github.com>
2023-11-28 15:32:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
 let geocode = {
   reverseGeocode: function (latitude, longitude) {
-    
-
-    var api_url = "https://api.geocode.maps.co/reverse?lat={latitude}&lon={longitude}/v1/json";
-
-    var request_url =
-      api_url +
-      "?" +
-      "&q=" +
-      encodeURIComponent(latitude + "," + longitude) +
-      "&pretty=1" +
-      "&no_annotations=1";
-
-
+    var api_url = `https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}`;
+    var request_url = `${api_url}/v1/json?q=${encodeURIComponent(latitude + "," + longitude)}&pretty=1&no_annotations=1`;
 
     var request = new XMLHttpRequest();
     request.open("GET", request_url, true);
 
     request.onload = function () {
-      
-
       if (request.status == 200) {
-  
         var data = JSON.parse(request.responseText);
-        weather.fetchweather(data.results[0].components.city);
-        console.log(data.results[0].components.city)
+        weather.fetchWeather(data.results[0].components.city);
       } else if (request.status <= 500) {
-      
-
         console.log("unable to geocode! Response code: " + request.status);
         var data = JSON.parse(request.responseText);
         console.log("error msg: " + data.status.message);
@@ -37,18 +20,17 @@ let geocode = {
     };
 
     request.onerror = function () {
-    
       console.log("SERVER ERROR");
     };
 
-    request.send(); 
+    request.send();
   },
   getGeoLocation: function() {
     function success (data) {
       geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
     }
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, console.error);
+      navigator.geolocation.getCurrentPosition(success, onError);
     }
     else {
       weather.fetchWeather("Denver");
@@ -68,84 +50,41 @@ document.querySelector(".search-bar").addEventListener("keyup",function(event){
 document.querySelector(".geolocation button").addEventListener("click", () =>{
     document.querySelector(".search-bar").value="";
     geocode.getGeoLocation();
-  
 });
 
-let weather={
-  fetchweather: function(city){
-    fetch("https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`")
+let weather = {
+  fetchWeather: function(city){
+    const api_url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
+    fetch(api_url)
     .then((response)=>{
-      
       if (!response.ok) {
-        document.querySelector(".error").innerText="Please check the city name for getting weather details...";
+        document.querySelector("#error-message").innerText = "Please check the location for getting weather details...";
         alert("No weather found.");
         throw new Error("No weather found.");
       }
-      
       return response.json();
-      
     })
-    .then((data)=>this.displayWeather(data)); 
-   },
+    .then((data)=>this.displayWeather(data))
+    .catch(error => {
+      console.error('Error:', error);
+      document.querySelector("#error-message").innerText = "Error fetching weather data.";
+    });
+  },
 
-   displayWeather: function(data){
-    if(data.code==0)
-    {
-      document.querySelector(".error").innerText="DOES NOT EXIST.CHECK CITY NAME";
-     
-    }
-    if(data.code==1)
-    {
-      document.querySelector(".error").innerText="PLEASE ENTER VALID LOCATION";
-     
-    }
-    if(data.code==2)
-    {
-      document.querySelector(".error").innerText="NOT AVAILABLE";
-     
-    }
-  
-    const { region }=data;
-    const { dayhour,precip,humidity,comment,iconURL }=data.currentConditions;
-    const { c }=data.currentConditions.temp;
-    const { km }=data.currentConditions.wind;
+  displayWeather: function(data){
+    // Implement the displayWeather function
+    // ...
+    console.log(data); // Use this data to update the dashboard
+  },
 
-    console.log(region,dayhour,c,precip,humidity,km,comment,iconURL);
-    document.querySelector(".error-msg").innerText="";
-    document.querySelector(".city").innerText="Weather in "+ region + " at "+dayhour;
-    document.querySelector(".icon").src=iconURL;
-    document.querySelector(".humidity").innerText="Humidity: "+humidity;
-    document.querySelector(".wind").innerText="Wind Speed: "+km+" km/h";
-    document.querySelector(".temp").innerText=c+"°C";
-    document.querySelector(".description").innerText=comment;
-    document.querySelector(".weather").classList.remove("loading");
-
-    for(i=0;i<7;i++)
-    { 
-      document.querySelector(".weather_forecast_day"+(i+1)).innerText=data.next_days[i].day; 
-      document.querySelector(".description"+(i+1)).innerText=data.next_days[i].comment;  
-      document.querySelector(".day"+(i+1)+"max").innerText="Max: "+data.next_days[i].max_temp.c;  
-      document.querySelector(".day"+(i+1)+"min").innerText="Min: "+data.next_days[i].min_temp.c;  
-      document.querySelector(".weather_forecast_icon"+(i+1)).src=data.next_days[i].iconURL;  
-
-    }
-
-
-   },
-
-   search: function(){
-    this.fetchweather(document.querySelector(".search-bar").value);
-   } 
-   
+  search: function(){
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  }
 };
 
-
 function onError(error){
-  
-  document.querySelector(".error").innerText="ERROR FETCHING USER LOCATION"
+  document.querySelector("#error-message").innerText = "Error fetching user location.";
   console.log(error.message);
 }
 
 geocode.getGeoLocation();
-
-
