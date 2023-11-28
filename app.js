ead9a71e1718cfb1ee4ec7c445ee3e4ab2bfc9bb
Harsh Jaiswal <145054490+hjaiswal99@users.noreply.github.com>
2023-11-28 12:14:51 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,75 @@
-// Function to handle location search
 function searchLocation() {
     const locationInput = document.getElementById('location-input').value;
 
-    // Use geocode API to get latitude and longitude for the entered location
+    if (locationInput.trim() === '') {
+        showError({ statusText: 'Please enter a location' });
+        return;
+    }
+
+    getLocationCoordinates(locationInput);
+}
+
+function getLocationCoordinates(location) {
     $.ajax({
         url: 'https://geocode.maps.co/',
         method: 'GET',
         data: {
-            address: locationInput
+            address: location
         },
         success: function(geocodeResponse) {
-            // Check if geocode response contains valid data
             if (geocodeResponse.results.length > 0) {
-                const location = geocodeResponse.results[0].geometry.location;
-
-                // Call the sunrise sunset API with the obtained latitude and longitude
-                getSunriseSunset(location.lat, location.lng);
+                const coordinates = geocodeResponse.results[0].geometry.location;
+                getSunriseSunset(coordinates.lat, coordinates.lng);
             } else {
-                // Show an error message for invalid location
                 showError({ statusText: 'Location not found' });
             }
         },
         error: function(error) {
-            // Handle errors from geocode API
             showError(error);
         }
     });
 }
 
-// Function to get sunrise and sunset data using latitude and longitude
+function getCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                getSunriseSunset(latitude, longitude);
+            },
+            function(error) {
+                handleError(error);
+            }
+        );
+    } else {
+        showError({ statusText: 'Geolocation is not supported by your browser' });
+    }
+}
+
+function handleError(error) {
+    let errorMessage;
+
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            errorMessage = 'User denied the request for geolocation.';
+            break;
+        case error.POSITION_UNAVAILABLE:
+            errorMessage = 'Location information is unavailable.';
+            break;
+        case error.TIMEOUT:
+            errorMessage = 'The request to get user location timed out.';
+            break;
+        case error.UNKNOWN_ERROR:
+            errorMessage = 'An unknown error occurred.';
+            break;
+        default:
+            errorMessage = 'An error occurred while getting user location.';
+    }
+
+    showError({ statusText: errorMessage });
+}
+
 function getSunriseSunset(latitude, longitude) {
     $.ajax({
         url: 'https://api.sunrise-sunset.org/json',
@@ -40,34 +81,21 @@ function getSunriseSunset(latitude, longitude) {
             formatted: 0
         },
         success: function(response) {
-            // Handle the successful response and update the dashboard
             updateDashboard(response);
         },
         error: function(error) {
-            // Handle errors and show an error message on the dashboard
             showError(error);
         }
     });
 }
 
-// Function to get current location and update the dashboard
-function getCurrentLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            function(position) {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
+function updateDashboard(data) {
+    // Implement the updateDashboard function
+    // ...
+}
 
-                // Call the sunrise sunset API with the obtained latitude and longitude
-                getSunriseSunset(latitude, longitude);
-            },
-            function(error) {
-                // Handle geolocation errors
-                showError({ statusText: `Geolocation error: ${error.message}` });
-            }
-        );
-    } else {
-        // Browser does not support geolocation
-        showError({ statusText: 'Geolocation is not supported by your browser' });
-    }
+function showError(error) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = `Error: ${error.statusText}`;
+    errorMessage.style.display = 'block';
 }
