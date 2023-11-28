16f9511d3e8896a0717e065b6fe53f257bb42ed3
Harsh Jaiswal <145054490+hjaiswal99@users.noreply.github.com>
2023-11-28 10:47:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // Example AJAX request using jQuery
 $.ajax({
-    url: 'https://sunrisesunset.io/api/',
+    url: 'https://api.sunrise-sunset.org/json',
     method: 'GET',
     data: {
         lat: 'your_latitude',
@@ -22,9 +22,37 @@ $.ajax({
 
 function updateDashboard(data) {
     // Update the HTML content with data from the API response
-    // You'll need to access the specific properties of the response object
+    const dashboard = document.getElementById('dashboard');
+    
+    // Extract relevant information from the response
+    const today = data.results;
+    const tomorrow = data.results;
+
+    // Display sunrise, sunset, dawn, dusk, day length, solar noon, and time zone information
+    dashboard.innerHTML = `
+        <h2>Today:</h2>
+        <p>Sunrise: ${today.sunrise}</p>
+        <p>Sunset: ${today.sunset}</p>
+        <p>Dawn: ${today.dawn}</p>
+        <p>Dusk: ${today.dusk}</p>
+        <p>Day Length: ${today.day_length} seconds</p>
+        <p>Solar Noon: ${today.solar_noon}</p>
+        <p>Time Zone: ${data.timezone}</p>
+
+        <h2>Tomorrow:</h2>
+        <p>Sunrise: ${tomorrow.sunrise}</p>
+        <p>Sunset: ${tomorrow.sunset}</p>
+        <p>Dawn: ${tomorrow.dawn}</p>
+        <p>Dusk: ${tomorrow.dusk}</p>
+        <p>Day Length: ${tomorrow.day_length} seconds</p>
+        <p>Solar Noon: ${tomorrow.solar_noon}</p>
+        <p>Time Zone: ${data.timezone}</p>
+    `;
 }
 
 function showError(error) {
     // Show an error message on the dashboard
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = `Error: ${error.statusText}`;
+    errorMessage.style.display = 'block';
 }
