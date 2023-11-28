9f926e541742c9b1f2d1556370d5c291e13ea3a8
Harsh Jaiswal <145054490+hjaiswal99@users.noreply.github.com>
2023-11-28 16:01:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// Coordinates for New York
-const latitude = 40.71427;
-const longitude = -74.00597;
-const url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
+function getSunriseSunset() {
+  const latitude = document.getElementById('latitude').value;
+  const longitude = document.getElementById('longitude').value;
 
-// Function to update the DOM with sunrise and sunset data
-function updateDashboard(data) {
-  document.querySelector('#sunrise-today').innerHTML = data.results.sunrise;
-  document.querySelector('#sunset-today').innerHTML = data.results.sunset;
-  document.querySelector('#raw-output').innerHTML = JSON.stringify(data);
+  // You may add additional validation for latitude and longitude inputs here
+
+  const apiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => displayResult(data))
+    .catch(error => console.error('Error:', error));
 }
 
-// Fetch data on page load
-fetch(url)
-  .then(response => response.json())
-  .then(data => updateDashboard(data))
-  .catch(error => console.error('Error:', error));
+function displayResult(data) {
+  const resultDiv = document.getElementById('result');
 
-// Add event listeners for search and current location buttons
-document.getElementById('search-btn').addEventListener('click', function () {
-  // Add logic to fetch data for the searched location and update the dashboard
-  // Use geocode API to get latitude and longitude for the searched location
-  // Update the 'url' variable with the new coordinates and fetch data
-});
-
-document.getElementById('current-location-btn').addEventListener('click', function () {
-  // Add logic to fetch data for the current location using the Geolocation API
-  // Update the 'url' variable with the new coordinates and fetch data
-});
+  if (data.status === 'OK') {
+    const results = data.results;
+    resultDiv.innerHTML = `
+      <p>Sunrise: ${results.sunrise}</p>
+      <p>Sunset: ${results.sunset}</p>
+      <p>First Light: ${results.first_light}</p>
+      <p>Last Light: ${results.last_light}</p>
+      <!-- Add more information as needed -->
+    `;
+  } else {
+    resultDiv.innerHTML = '<p>Error retrieving data. Please try again.</p>';
+  }
+}
