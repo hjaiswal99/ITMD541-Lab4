a9ef05e6fb17d7f5e73228b59f006a1635290b9a
Harsh Jaiswal <145054490+hjaiswal99@users.noreply.github.com>
2023-11-28 15:55:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,29 @@
-let geocode = {
-  reverseGeocode: function (latitude, longitude) {
-    var api_url = `https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}`;
-    var request_url = `${api_url}/v1/json?q=${encodeURIComponent(latitude + "," + longitude)}&pretty=1&no_annotations=1`;
+// Coordinates for New York
+const latitude = 40.71427;
+const longitude = -74.00597;
+const url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
 
-    var request = new XMLHttpRequest();
-    request.open("GET", request_url, true);
-
-    request.onload = function () {
-      if (request.status == 200) {
-        var data = JSON.parse(request.responseText);
-        weather.fetchWeather(data.results[0].components.city);
-      } else if (request.status <= 500) {
-        console.log("unable to geocode! Response code: " + request.status);
-        var data = JSON.parse(request.responseText);
-        console.log("error msg: " + data.status.message);
-      } else {
-        console.log("server error");
-      }
-    };
-
-    request.onerror = function () {
-      console.log("SERVER ERROR");
-    };
-
-    request.send();
-  },
-  getGeoLocation: function() {
-    function success (data) {
-      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
-    }
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, onError);
-    }
-    else {
-      weather.fetchWeather("Denver");
-    }
-  }
-};
-
-document.querySelector(".search button").addEventListener("click",function(){
-  weather.search();
-})
-
-document.querySelector(".search-bar").addEventListener("keyup",function(event){
-  if(event.key=="Enter")
-  weather.search();
-});
-
-document.querySelector(".geolocation button").addEventListener("click", () =>{
-    document.querySelector(".search-bar").value="";
-    geocode.getGeoLocation();
-});
-
-let weather = {
-  fetchWeather: function(city){
-    const api_url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
-    fetch(api_url)
-    .then((response)=>{
-      if (!response.ok) {
-        document.querySelector("#error-message").innerText = "Please check the location for getting weather details...";
-        alert("No weather found.");
-        throw new Error("No weather found.");
-      }
-      return response.json();
-    })
-    .then((data)=>this.displayWeather(data))
-    .catch(error => {
-      console.error('Error:', error);
-      document.querySelector("#error-message").innerText = "Error fetching weather data.";
-    });
-  },
-
-  displayWeather: function(data){
-    // Implement the displayWeather function
-    // ...
-    console.log(data); // Use this data to update the dashboard
-  },
-
-  search: function(){
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  }
-};
-
-function onError(error){
-  document.querySelector("#error-message").innerText = "Error fetching user location.";
-  console.log(error.message);
+// Function to update the DOM with sunrise and sunset data
+function updateDashboard(data) {
+  document.querySelector('#sunrise-today').innerHTML = data.results.sunrise;
+  document.querySelector('#sunset-today').innerHTML = data.results.sunset;
+  document.querySelector('#raw-output').innerHTML = JSON.stringify(data);
 }
 
-geocode.getGeoLocation();
+// Fetch data on page load
+fetch(url)
+  .then(response => response.json())
+  .then(data => updateDashboard(data))
+  .catch(error => console.error('Error:', error));
+
+// Add event listeners for search and current location buttons
+document.getElementById('search-btn').addEventListener('click', function () {
+  // Add logic to fetch data for the searched location and update the dashboard
+  // Use geocode API to get latitude and longitude for the searched location
+  // Update the 'url' variable with the new coordinates and fetch data
+});
+
+document.getElementById('current-location-btn').addEventListener('click', function () {
+  // Add logic to fetch data for the current location using the Geolocation API
+  // Update the 'url' variable with the new coordinates and fetch data
+});
