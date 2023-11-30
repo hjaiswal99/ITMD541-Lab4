955c8cfc08b420eb1079e53d6e6c147573059aba
Harsh Jaiswal <145054490+hjaiswal99@users.noreply.github.com>
2023-11-29 19:58:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,136 +1,48 @@
+// script.js
+
 function getSunriseSunset() {
-  // Clear previous results
-  clearResults();
+  // Get latitude and longitude values from input fields
+  const latitudeInput = document.getElementById('latitude');
+  const longitudeInput = document.getElementById('longitude');
 
-  const latitude = document.getElementById('latitude').value;
-  const longitude = document.getElementById('longitude').value;
+  const latitude = latitudeInput.value;
+  const longitude = longitudeInput.value;
 
-  // You may add additional validation for latitude and longitude inputs here
-
-  const todayUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
-  const tomorrowUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=tomorrow`;
-
-  // Fetch data for today
-  fetch(todayUrl)
-    .then(response => response.json())
-    .then(data => displayResult('Today', data))
-    .catch(error => {
-      console.error('Error fetching today\'s data:', error);
-      displayError();
-    });
-
-  // Fetch data for tomorrow
-  fetch(tomorrowUrl)
-    .then(response => response.json())
-    .then(data => displayResult('Tomorrow', data))
-    .catch(error => {
-      console.error('Error fetching tomorrow\'s data:', error);
-      displayError();
-    });
-}
-
-function clearResults() {
-  document.getElementById('result').innerHTML = '';
-}
-
-function displayError() {
+  // Call a function to fetch sunrise and sunset data using the provided latitude and longitude
+  // Display the result in the "result" div
   const resultDiv = document.getElementById('result');
-  resultDiv.innerHTML = '<p>Error retrieving data. Please try again.</p>';
-}
+  resultDiv.innerHTML = `Fetching sunrise and sunset data for latitude ${latitude} and longitude ${longitude}...`;
 
-function displayResult(day, data) {
-  const resultDiv = document.getElementById('result');
+  // Example: You can use an API to fetch sunrise and sunset data
+  // Replace the URL with the actual API endpoint you plan to use
+  const apiUrl = `https://api.example.com/sunrise-sunset?lat=${latitude}&lon=${longitude}`;
 
-  if (data.status === 'OK') {
-    const results = data.results;
-    resultDiv.innerHTML += `<h2>${day}</h2>`;
-    resultDiv.innerHTML += `
-      <p>Sunrise: ${results.sunrise}</p>
-      <p>Sunset: ${results.sunset}</p>
-      <p>Dawn: ${results.dawn}</p>
-      <p>Dusk: ${results.dusk}</p>
-      <p>Day Length: ${results.day_length}</p>
-      <p>Solar Noon: ${results.solar_noon}</p>
-      <p>Timezone: ${results.timezone}</p>
-      <hr>`;
-  } else {
-    resultDiv.innerHTML += `<h2>${day}</h2>`;
-    resultDiv.innerHTML += '<p>Error retrieving data. Please try again.</p>';
-    resultDiv.innerHTML += '<hr>';
-  }
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Update the resultDiv with the fetched data
+      resultDiv.innerHTML = `Sunrise: ${data.sunrise}, Sunset: ${data.sunset}`;
+      // Clear input fields after successful fetch
+      latitudeInput.value = '';
+      longitudeInput.value = '';
+    })
+    .catch(error => {
+      // Handle errors
+      resultDiv.innerHTML = `Error fetching data: ${error.message}`;
+    });
 }
 
 function getCurrentLocation() {
-  // Clear previous results
-  clearResults();
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        document.getElementById('latitude').value = latitude;
-        document.getElementById('longitude').value = longitude;
-        getSunriseSunset();
-      },
-      error => {
-        console.error('Error getting current location:', error);
-        alert('Unable to retrieve your location. Please enter manually.');
-      }
-    );
-  } else {
-    alert('Geolocation is not supported by your browser. Please enter manually.');
-  }
+  // Implement code to get the user's current location and update the latitude and longitude input fields
+  // Clear input fields after setting current location
+  document.getElementById('latitude').value = '';
+  document.getElementById('longitude').value = '';
 }
 
-// Corrected searchLocation function
 function searchLocation() {
-  const cityInput = document.getElementById('locationQuery');
-  const resultElement = document.getElementById('result');
-
-  // Check if the input element exists
-  if (!cityInput) {
-    console.error('Error: Unable to find input element with ID "locationQuery".');
-    return;
-  }
-
-  const city = cityInput.value.trim();
-
-  if (city === '') {
-    resultElement.textContent = 'Please enter a city.';
-    return;
-  }
-
-  const apiUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(city)}`;
-
-  fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('API Response:', data);
-
-      if (data && data.length > 0) {
-        const firstResult = data[0];
-        if (firstResult.lat && firstResult.lon) {
-          const latitude = firstResult.lat;
-          const longitude = firstResult.lon;
-          const displayName = firstResult.display_name;
-          resultElement.textContent = `Location of ${displayName}: Latitude ${latitude}, Longitude ${longitude}`;
-        } else {
-          resultElement.textContent = 'Latitude and Longitude information not available for the first result.';
-        }
-      } else {
-        resultElement.textContent = 'Location not found.';
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-      resultElement.textContent = 'An error occurred while fetching data.';
-    });
+  // Implement code to search for a location using the input in the "locationQuery" field
+  // Update the latitude and longitude input fields with the results
+  // Clear input fields after searching location
+  document.getElementById('latitude').value = '';
+  document.getElementById('longitude').value = '';
 }
-
-// Rest of your existing code...
